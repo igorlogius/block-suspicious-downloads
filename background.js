f027bde0df6e84375d5d670da65b2bc5fe7b7d34
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-04-07 17:53:36 +0200

    chore: cleanup

--- background.js
+++ background.js
@@ -1,9 +1,55 @@
 /* global browser */
 
 let enabled = true;
+//const regex = /\.(exe|wmi|dll|sfx|sea)/gim;
+
+function getSuspiciousSubstrings(str) {
+  str = str.toLowerCase();
+  return [
+    "exe",
+    "wmi",
+    "dll",
+    "sfx",
+    "sea",
+    "scr",
+    "bat",
+    "bin",
+    "cmd",
+    "com",
+    "cpl",
+    "csh",
+    "ex_",
+    "gadget",
+    "inf",
+    "ins",
+    "ipa",
+    "isu",
+    "job",
+    "jse",
+    "lnk",
+    "msc",
+    "msi",
+    "msp",
+    "mst",
+    "paf",
+    "pif",
+    "ps1",
+    "reg",
+    "rgs",
+    "vb",
+    "vbe",
+    "vbs",
+    "vbscript",
+    "ws",
+    "wsf",
+    "wsh",
+  ].filter((ext) => str.includes("." + ext));
+}
 
 browser.downloads.onCreated.addListener(async (item) => {
-  if (item.filename.includes(".exe")) {
+  const matches = getSuspiciousSubstrings(item.filename);
+
+  if (matches.length) {
     try {
       // first we try to cancel the download
       await browser.downloads.cancel(item.id);
@@ -12,8 +58,7 @@ browser.downloads.onCreated.addListener(async (item) => {
         type: "basic",
         iconUrl: browser.runtime.getURL("warn.png"),
         title: "Download Canceled",
-        message:
-          "The download was canceled because its filename contained the suspicious substring '.exe'",
+        message: "filename seemed suspicious [" + matches.join(",") + "]",
       });
     } catch (e) {
       console.error(e);
@@ -25,18 +70,22 @@ browser.downloads.onCreated.addListener(async (item) => {
           priority: 2,
           type: "basic",
           iconUrl: browser.runtime.getURL("warn.png"),
-          title: "File Removed",
-          message:
-            "The download file was removed because its filename contained the suspicious substring '.exe'",
+          title: "Download Removed",
+          message: "filename seemed suspicious [" + matches.join(",") + "]",
         });
       } catch (e) {
         console.error(e);
+        // now if that did not work ... lets at least try and warn the user with a notification
+        // todo noop for now
       }
     }
   }
-  enabled = true;
-  browser.browserAction.setBadgeText({ text: "ON" });
-  browser.browserAction.setBadgeBackgroundColor({ color: "green" });
+  // renenable after 3 minutes
+  setTimeout(() => {
+    enabled = true;
+    browser.browserAction.setBadgeText({ text: "ON" });
+    browser.browserAction.setBadgeBackgroundColor({ color: "green" });
+  }, 3 * 60 * 1000);
 });
 
 browser.browserAction.onClicked.addListener(() => {
